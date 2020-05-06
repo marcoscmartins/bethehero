153607f44d7a4ea2530974dd2676696369ffa93b
Marcos Cardoso <dev.marcoscardoso@gmail.com>
2020-05-05 23:46:57 -0300

    DB Connection
    Store new ongs and listing all ongs.

---
+++ backend/src/database/connection.js
@@ -0,0 +1,9 @@
+// import do knex
+const knex = require('knex');
+// import das configurações do DB
+const configuration = require('../../knexfile');
+
+// criação da conexão utiliando o knex e passando o configuration.development
+const connection = knex(configuration.development);
+
+module.exports = connection;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,32 @@
 // importo todas as funcionalidades do express
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 // Módulo de rotas do express
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-	const body = request.body;
-	console.log(body);
+routes.get('/ongs', async (request, response) => 
+{
+	const ongs = await connection('ongs').select('*');
+	return response.json(ongs);
+});
 
-	return response.json({
-		evento: 'Semana OmniStack 11.0',
-		aluno: 'Marcos Cardoso Martins'
+routes.post('/ongs', async (request, response) => {
+	const { name, email, whatsapp, city, uf } = request.body;
+
+	const id = crypto.randomBytes(4).toString('HEX');
+
+	await connection('ongs').insert({
+		id, 
+		name,
+		email,
+		whatsapp,
+		city,
+		uf,
 	});
+	
+	return response.json( { id } );
 });
 
 // Isso é o que torna a rota visivel dentro da aplicação
